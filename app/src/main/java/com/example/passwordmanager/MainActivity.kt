2b410c91fd6d45acf61cee7c4ce34d1b8f877ea8
joshua <joshua.heemsbergen@gmail.com>
2026-05-07 03:36:41 +0200

    commit

--- app/src/main/java/com/example/passwordmanager/MainActivity.kt
+++ app/src/main/java/com/example/passwordmanager/MainActivity.kt
@@ -1,17 +1,45 @@
 package com.example.passwordmanager
 
 import android.os.Bundle
+import android.text.style.BackgroundColorSpan
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.text.isDigitsOnly
 import com.example.passwordmanager.ui.theme.PasswordManagerTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,30 +48,59 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             PasswordManagerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    PasswordScreen()
+                    }
                 }
             }
         }
     }
-}
-
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        fontSize = 30.sp,
-        modifier = modifier
-    )
+fun PasswordScreen(){
+    var password by remember { mutableStateOf("") }
+    val length = 4
+    Box(modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Text(
+                text = "Pin", color = Color.White,
+                style = TextStyle(fontSize = 37.sp),
+                textAlign = TextAlign.Center,
+                modifier = Modifier.fillMaxWidth()
+            )
+            TextField(
+                textStyle = TextStyle(textAlign = TextAlign.Center, fontSize = 24.sp),
+                value = password,
+                modifier = Modifier.width(120.dp),
+                onValueChange = { input ->
+                    password = input
+                        .filter { password.isDigit() }.take(length)
+                })
+        }
+    }
 }
-
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun GreetingPreview() {
-    PasswordManagerTheme {
-        Greeting(name="Android")
+fun PreviewPasswordScreen(){
+    var password by remember { mutableStateOf("") }
+    val length = 4
+    Box(modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Text(
+                text = "Pin", color = Color.White,
+                style = TextStyle(fontSize = 37.sp),
+                textAlign = TextAlign.Center,
+                modifier = Modifier.fillMaxWidth()
+            )
+            TextField(
+                textStyle = TextStyle(textAlign = TextAlign.Center, fontSize = 24.sp),
+                value = password,
+                modifier = Modifier.width(120.dp),
+                onValueChange = { input ->
+                    password = input
+                        .filter { password.isDigitsOnly() }.take(length)
+                })
+        }
     }
 }
